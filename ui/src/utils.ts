49e11ee674e716c49c693bb7b481df951a282bb4
Ryan Cunningham <cham.ryan@gmail.com>
2021-09-25 22:31:26 -0400

    Move to LIveView app only, iFrame setup

--- ui/src/utils.ts
+++
@@ -1,9 +0,0 @@
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-export const uuidv4 = (a?: any, b?: any): string => {
-    for (
-        b = a = '';
-        a++ < 36;
-        b += ~a % 5 | ((a * 3) & 4) ? (a ^ 15 ? 8 ^ (Math.random() * (a ^ 20 ? 16 : 4)) : 4).toString(16) : '-'
-    );
-    return b;
-};
--- ui/src/utils/socket.ts
+++
@@ -1,6 +0,0 @@
-import { Socket } from 'phoenix';
-
-const socket = new Socket('ws://localhost:4040/socket', { params: { userToken: '123' } });
-socket.connect();
-
-export default socket;
