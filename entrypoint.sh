72f0201c7a009012dc0a9d95a6bdae4f51f37bc7
Ryan Cunningham <cham.ryan@gmail.com>
2021-10-06 18:57:51 -0400

    Not updating?

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,8 +4,8 @@
 # wait until Postgres is ready
 while ! pg_isready -q -d $DATABASE_URL
 do
-  echo "$(date) - waiting for database to start"
-  sleep 2
+  echo "$(date) - waiting for database to start..."
+  sleep 5
 done
 
 bin="/app/bin/commentator"
