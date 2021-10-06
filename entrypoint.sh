13cfe1c946b3bb1cce51e51f3afb59e1d312ea92
Ryan Cunningham <cham.ryan@gmail.com>
2021-10-06 18:40:24 -0400

    Pull the connection string

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 # docker entrypoint script.
 
-# assign a default for the database_user
-DB_USER=${DATABASE_USER:-postgres}
-
 # wait until Postgres is ready
-while ! pg_isready -q -h $DATABASE_HOST -p 5432 -U $DB_USER
+while ! pg_isready -q -d $DATABASE_URL
 do
   echo "$(date) - waiting for database to start"
   sleep 2
