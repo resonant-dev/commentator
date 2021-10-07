b2a5c75071506d46b53dd8df81a11116cfce24cd
Ryan Cunningham <cham.ryan@gmail.com>
2021-10-07 08:35:42 -0400

    Wait for ssl on migrations

--- lib/commentator/release.ex
+++ lib/commentator/release.ex
@@ -3,12 +3,16 @@ defmodule Commentator.Release do
   @app :commentator
 
   def migrate do
+    Application.ensure_started(:ssl)
+
     for repo <- repos() do
       {:ok, _, _} = Ecto.Migrator.with_repo(repo, &Ecto.Migrator.run(&1, :up, all: true))
     end
   end
 
   def rollback(repo, version) do
+    Application.ensure_started(:ssl)
+
     {:ok, _, _} = Ecto.Migrator.with_repo(repo, &Ecto.Migrator.run(&1, :down, to: version))
   end
 
