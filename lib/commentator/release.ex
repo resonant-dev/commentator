ae3c6dd1a7990486e321aaaaa63bdba3edc157f2
Ryan Cunningham <cham.ryan@gmail.com>
2021-10-07 10:26:31 -0400

    Hmph

--- lib/commentator/release.ex
+++ lib/commentator/release.ex
@@ -1,16 +1,19 @@
 defmodule Commentator.Release do
   @moduledoc false
   @app :commentator
+  require Logger
 
   def migrate do
+    Logger.info("Running migrations...")
+
     for repo <- repos() do
-      Application.ensure_started(:ssl)
+      Application.ensure_all_started(@app)
       {:ok, _, _} = Ecto.Migrator.with_repo(repo, &Ecto.Migrator.run(&1, :up, all: true))
     end
   end
 
   def rollback(repo, version) do
-    Application.ensure_started(:ssl)
+    Application.ensure_all_started(@app)
 
     {:ok, _, _} = Ecto.Migrator.with_repo(repo, &Ecto.Migrator.run(&1, :down, to: version))
   end
